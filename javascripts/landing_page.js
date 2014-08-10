a7347a6cba12584f227ae22ed6898529ff393518
Ethan Gould <ethan.g.907@gmail.com>
2014-08-10 16:59:18 -0400

    added functions to show and hide search filter options

--- javascripts/landing_page.js
+++ javascripts/landing_page.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-  $(".dropdown").hover(App.showOptions)
+  $(".dropdown").hover(App.showTopLinkOptions)
+  $("#size").click(App.showSizeOptions)
+  $("#color").click(App.showColorOptions)
+  $("#price").click(App.showPriceOptions)
 });
 
 var App = App || {};
 
-App.showOptions = function(){
+App.showTopLinkOptions = function(){
   $(this).children('ul').toggle();
+}
+
+App.showSizeOptions = function(){
+  $options = $('.size-filter-options');
+  $options.slideToggle("fast");
+}
+
+App.showColorOptions = function(){
+  $options = $('.color-filter-options');
+  $options.slideToggle("fast");
+}
+
+App.showPriceOptions = function(){
+  $options = $('.price-filter-options');
+  $options.slideToggle("fast");
 }
